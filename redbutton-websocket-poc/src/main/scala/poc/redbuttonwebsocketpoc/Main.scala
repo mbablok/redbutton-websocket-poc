d5b8d8217d25c795073f49df303f1eb850432dcf
Marcin Bablok <marcin.bablok@itv.com>
2025-11-13 16:00:48 +0100

    Removed receiving, sending some static reactions every second

--- redbutton-websocket-poc/src/main/scala/poc/redbuttonwebsocketpoc/Main.scala
+++ redbutton-websocket-poc/src/main/scala/poc/redbuttonwebsocketpoc/Main.scala
@@ -5,6 +5,7 @@ import cats.effect.{IO, IOApp}
 import fs2.concurrent.Topic
 import fs2.Stream
 import org.http4s.websocket.WebSocketFrame
+import scala.concurrent.duration.*
 
 object Main extends IOApp.Simple:
   def program: IO[Unit] = {
@@ -13,9 +14,17 @@ object Main extends IOApp.Simple:
       t <- Topic[IO, WebSocketFrame]
       s <- Stream(
         Stream.fromQueueUnterminated(q).through(t.publish),
-        Stream.eval(RedbuttonwebsocketpocServer.run[IO](q, t))
+        Stream.eval(RedbuttonwebsocketpocServer.run[IO](q, t)),
+        Stream
+          .awakeEvery[IO](1.seconds)
+          .map(_ =>
+            WebSocketFrame.Text(
+              """{"reactions":{"reaction-1":22,"reaction-2":6,"reaction-3":5,"reaction-4":15,"reaction-5":7}}
+            """
+            )
+          )
+          .through(t.publish)
       ).parJoinUnbounded.compile.drain
     } yield s
   }
   override def run: IO[Unit] = program
-
--- redbutton-websocket-poc/src/main/scala/poc/redbuttonwebsocketpoc/Routes.scala
+++ redbutton-websocket-poc/src/main/scala/poc/redbuttonwebsocketpoc/Routes.scala
@@ -11,24 +11,22 @@ import org.http4s.dsl.Http4sDsl
 import org.http4s.server.websocket.WebSocketBuilder2
 import org.http4s.websocket.WebSocketFrame
 
-
 class Routes[F[_]: Files: Concurrent] extends Http4sDsl[F] {
-  def service (
-                wsb: WebSocketBuilder2[F],
-                q: Queue[F, WebSocketFrame],
-                t: Topic[F, WebSocketFrame]
-              ): HttpApp[F] = {
-    HttpRoutes.of[F] {
-      case GET -> Root / "ws" =>
-        val send: Stream[F, WebSocketFrame] = {
-          t.subscribe(maxQueued = 1000)
-        }
+  def service(
+      wsb: WebSocketBuilder2[F],
+      q: Queue[F, WebSocketFrame],
+      t: Topic[F, WebSocketFrame]
+  ): HttpApp[F] = {
+    HttpRoutes.of[F] { case GET -> Root / "ws" =>
+      val send: Stream[F, WebSocketFrame] = {
+        t.subscribe(maxQueued = 1000)
+      }
 
-        val receive: Pipe[F, WebSocketFrame, Unit] = {
-          _.foreach(q.offer)
-        }
+      val receive: Pipe[F, WebSocketFrame, Unit] = {
+        _.foreach { _ => Concurrent[F].unit }
+      }
 
-        wsb.build(send, receive)
+      wsb.build(send, receive)
     }
   }.orNotFound
 }
