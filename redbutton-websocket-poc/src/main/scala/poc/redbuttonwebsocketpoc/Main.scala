624affd924a230e318f432700976cbbbfefe40fc
MunaSHassan <muna.hassan@itv.com>
2025-11-13 16:22:40 +0000

    use case class for reactions and add encoder and decoder

--- redbutton-websocket-poc/src/main/scala/poc/redbuttonwebsocketpoc/Main.scala
+++ redbutton-websocket-poc/src/main/scala/poc/redbuttonwebsocketpoc/Main.scala
@@ -7,8 +7,37 @@ import fs2.Stream
 import org.http4s.websocket.WebSocketFrame
 import scala.concurrent.duration.*
 import scala.util.Random
+import io.circe.{Encoder, Decoder, Json}
+import io.circe.syntax.*
 
 object Main extends IOApp.Simple:
+
+  case class Reactions(
+                        reaction1: Int,
+                        reaction2: Int,
+                        reaction3: Int,
+                        reaction4: Int,
+                        reaction5: Int
+                      )
+
+  given Encoder[Reactions] = Encoder.instance { r =>
+    Json.obj(
+      "reaction-1" -> Json.fromInt(r.reaction1),
+      "reaction-2" -> Json.fromInt(r.reaction2),
+      "reaction-3" -> Json.fromInt(r.reaction3),
+      "reaction-4" -> Json.fromInt(r.reaction4),
+      "reaction-5" -> Json.fromInt(r.reaction5)
+    )
+  }
+  given Decoder[Reactions] = Decoder.instance { c =>
+    for {
+      reaction1 <- c.downField("reaction-1").as[Int]
+      reaction2 <- c.downField("reaction-2").as[Int]
+      reaction3 <- c.downField("reaction-3").as[Int]
+      reaction4 <- c.downField("reaction-4").as[Int]
+      reaction5 <- c.downField("reaction-5").as[Int]
+    } yield Reactions(reaction1, reaction2, reaction3, reaction4, reaction5)
+  }
 
   def program: IO[Unit] = {
     for {
@@ -37,7 +66,7 @@ object Main extends IOApp.Simple:
               (r1, r2, r3, r4, r5) = reactionsCount
               _ <- IO.sleep(delay.millis)
             } yield WebSocketFrame.Text(
-              s"""{"reactions":{"reaction-1":$r1,"reaction-2":$r2,"reaction-3":$r3,"reaction-4":$r4,"reaction-5":$r5}}"""
+              Reactions(r1, r2, r3, r4, r5).asJson.noSpaces
             )
           )
           .through(t.publish)
